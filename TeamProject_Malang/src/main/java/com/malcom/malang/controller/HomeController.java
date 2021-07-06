192ed3589b092094484c8eebfe8f246f74b9959c
sk421120 <sk421120@naver.com>
2021-07-06 18:07:11 +0900

    오늘의 말랑 완성

--- TeamProject_Malang/src/main/java/com/malcom/malang/controller/HomeController.java
+++ TeamProject_Malang/src/main/java/com/malcom/malang/controller/HomeController.java
@@ -1,17 +1,30 @@
 package com.malcom.malang.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.malcom.malang.model.ItemVO;
+import com.malcom.malang.service.ItemService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+	
+	protected final ItemService itService;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home() {
+	public String home(Model model) {
+		List<ItemVO> todayMalang = itService.malangOfToday();
+		
+		model.addAttribute("ITEM_LIST",todayMalang);
 		return "home";
 	}		
 	
--- TeamProject_Malang/src/main/webapp/WEB-INF/views/include/home_list.jsp
+++ TeamProject_Malang/src/main/webapp/WEB-INF/views/include/home_list.jsp
@@ -1,16 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Insert title here</title>
-<link rel="stylesheet" type="text/css" href="${rootPath}/static/css/font.css?ver=2021-06-14-001" />
-<link rel="stylesheet"
-	href="${rootPath}/static/css/list.css?ver=2021-06-14-2000" />
-</head>
-<body>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<c:set var="rootPath" value="${pageContext.request.contextPath}" />
+<link rel="stylesheet" href="${rootPath}/static/css/list.css?ver=2021-06-14-2000" />
 <div class="today"><h3>오늘의 말랑</h3></div>
 <%@ include file="/WEB-INF/views/include/list.jsp" %>
-</body>
-</html>
