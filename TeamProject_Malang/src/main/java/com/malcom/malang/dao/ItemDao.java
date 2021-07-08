2f87d1aa861afdbb7cfa5e7e94869240e190382a
sk421120 <sk421120@naver.com>
2021-07-09 06:38:10 +0900

    categoryList_ing

--- TeamProject_Malang/src/main/java/com/malcom/malang/dao/ItemDao.java
+++ TeamProject_Malang/src/main/java/com/malcom/malang/dao/ItemDao.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import com.malcom.malang.model.CateVO;
 import com.malcom.malang.model.ItemVO;
 
 public interface ItemDao extends GenericDao<ItemVO, String> {
 
+	// category select
+	public List<CateVO> selectCategory();
 	public List<String> categoryMain();
 	public List<String> categorySub(@Param("ct_main") String ct_main);
+	
+	public List<CateVO> categorySubWithCode(@Param("ct_main") String ct_main);
 }
