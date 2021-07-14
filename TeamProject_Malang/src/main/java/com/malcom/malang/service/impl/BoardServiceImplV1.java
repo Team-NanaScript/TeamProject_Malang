6cb5a32e465b4a21d8a797c059828a2656d8ea50
kimyoungjin98 <gyu250@naver.com>
2021-07-14 14:27:16 +0900

    보드수정

--- TeamProject_Malang/src/main/java/com/malcom/malang/service/impl/BoardServiceImplV1.java
+++ TeamProject_Malang/src/main/java/com/malcom/malang/service/impl/BoardServiceImplV1.java
@@ -30,8 +30,8 @@ public class BoardServiceImplV1 implements BoardService{
 	public Integer insert(BoardVO vo) {
 		// TODO Auto-generated method stub
 		
-		String sDate = DateConfig.sDate("yyyy-mm-dd");
-		String sTime = DateConfig.stime("hh:MM:ss");
+		String sDate = DateConfig.sDate("yyyy-MM-dd");
+		String sTime = DateConfig.stime("HH:mm:ss");
 		
 		vo.setBd_date(sDate);
 		vo.setBd_time(sTime);
@@ -45,8 +45,8 @@ public class BoardServiceImplV1 implements BoardService{
 	public Integer update(BoardVO vo) {
 		
 		
-		String sDate = DateConfig.sDate("yyyy-mm-dd");
-		String sTime = DateConfig.stime("hh:MM:ss");
+		String sDate = DateConfig.sDate("yyyy-MM-dd");
+		String sTime = DateConfig.stime("HH:mm:ss");
 		
 		vo.setBd_date(sDate);
 		vo.setBd_time(sTime);
